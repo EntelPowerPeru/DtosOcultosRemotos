d4a3274954c619a5afce4653f7dd6ba4906d6a28
EntelPowerPeru <leonidaaalarcon@gmail.com>
2024-03-22 00:13:05 -0500

    VFinal

--- scripts/cal-in.js
+++ scripts/cal-in.js
@@ -3,21 +3,27 @@ const EVENTOS = {
         this.seleccionarTipoCliente();
     },
     seleccionarTipoCliente() {
-        $("body").on("change", "#filtro_cliente", (evento) => {
-            const valor = $(evento.currentTarget).val();
+        $("body").on("change", "#filtro_cliente, #filtro_canal", (evento) => {
+            const valorCliente = $("#filtro_cliente").val();
+            const valorCanal = $("#filtro_canal").val();
 
-            switch (valor) {
-                case "1":
-                    $("#tb_nuevo").removeClass("d-none");
-                    $("#tb_entel").addClass("d-none");
-                    break;
-                case "2":
-                    $("#tb_nuevo").addClass("d-none");
-                    $("#tb_entel").removeClass("d-none");
-                    break;
-                default:
-                    $("#tb_nuevo").addClass("d-none");
-                    $("#tb_entel").addClass("d-none");
+            if (valorCanal === "4") {
+                switch (valorCliente) {
+                    case "1":
+                        $("#tb_nuevo").removeClass("d-none");
+                        $("#tb_entel").addClass("d-none");
+                        break;
+                    case "2":
+                        $("#tb_nuevo").addClass("d-none");
+                        $("#tb_entel").removeClass("d-none");
+                        break;
+                    default:
+                        $("#tb_nuevo").addClass("d-none");
+                        $("#tb_entel").addClass("d-none");
+                }
+            } else {
+                $("#tb_nuevo").addClass("d-none");
+                $("#tb_entel").addClass("d-none");
             }
         });
     }
--- scripts/digital-asistido-app.js
+++ scripts/digital-asistido-app.js
@@ -3,23 +3,29 @@ const EVENTOS = {
         this.seleccionarTipoCliente();
     },
     seleccionarTipoCliente() {
-        $("body").on("change", "#filtro_cliente", (evento) => {
-            const valor = $(evento.currentTarget).val();
+        $("body").on("change", "#filtro_cliente, #filtro_canal", (evento) => {
+            const valorCliente = $("#filtro_cliente").val();
+            const valorCanal = $("#filtro_canal").val();
 
-            switch (valor) {
-                case "1":
-                    $("#tb_nuevo").removeClass("d-none");
-                    $("#tb_entel").addClass("d-none");
-                    $(".lbl-transaccion").html("Portabilidad");
-                    break;
-                case "2":
-                    $("#tb_nuevo").addClass("d-none");
-                    $("#tb_entel").removeClass("d-none");
-                    $(".lbl-transaccion").html("Portabilidad y Venta Regular");
-                    break;
-                default:
-                    $("#tb_nuevo").addClass("d-none");
-                    $("#tb_entel").addClass("d-none");
+            if (valorCanal === "4") {
+                switch (valorCliente) {
+                    case "1":
+                        $("#tb_nuevo").removeClass("d-none");
+                        $("#tb_entel").addClass("d-none");
+                        $(".lbl-transaccion").html("Portabilidad");
+                        break;
+                    case "2":
+                        $("#tb_nuevo").addClass("d-none");
+                        $("#tb_entel").removeClass("d-none");
+                        $(".lbl-transaccion").html("Portabilidad y Venta Regular");
+                        break;
+                    default:
+                        $("#tb_nuevo").addClass("d-none");
+                        $("#tb_entel").addClass("d-none");
+                }
+            } else {
+                $("#tb_nuevo").addClass("d-none");
+                $("#tb_entel").addClass("d-none");
             }
         });
     }
